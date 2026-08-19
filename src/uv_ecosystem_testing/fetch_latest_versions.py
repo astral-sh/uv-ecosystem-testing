f831fd650daa8c3118910439e4093a433abbea86
William Woodruff <william@yossarian.net>
2026-08-19 14:22:40 -0400

    Use stdlib JSON parser
    Signed-off-by: William Woodruff <william@yossarian.net>

--- src/uv_ecosystem_testing/fetch_latest_versions.py
+++ src/uv_ecosystem_testing/fetch_latest_versions.py
@@ -5,9 +5,9 @@ Fetch the latest version for each package in the input file.
 import argparse
 import asyncio
 import csv
+import json
 from pathlib import Path
 
-import orjson
 from httpx import AsyncClient, HTTPError
 from tqdm.asyncio import tqdm
 
@@ -20,7 +20,7 @@ async def get_latest_version(
     try:
         response = await client.get(f"https://pypi.org/pypi/{package_name}/json")
         response.raise_for_status()
-        data = orjson.loads(response.content)
+        data = json.loads(response.content)
         return package_name, data["info"]["version"]
     except HTTPError as e:
         print(f"Error fetching latest version for {package_name}: {e}")
